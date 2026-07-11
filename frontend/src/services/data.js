f23f911183e209c3cfe672feb8f01c56de1f6ec6
Prem <prempawar241@gmail.com>
2026-07-11 11:47:06 +0530

    Updated subGfits from kokohaan ( anchan ) the UI work properly and show the reamaing sub gifts

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -9,7 +9,7 @@ import TenZAvatar from "../assets/Tenz.png";
 export const channels = [
   {
     name: "心羽あん",
-    subs: 4330,
+    subs: 4346,
     bits: 50645,
     avatar: kokohaanAvatar,
     twitchUrl: "https://www.twitch.tv/kokohaan",
