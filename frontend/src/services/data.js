0f9cf31598a179ba2b89c021fe51684a19d1691f
Prem <prempawar241@gmail.com>
2026-07-22 15:25:09 +0530

    Updated subGfits from anchan

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -9,7 +9,7 @@ import TenZAvatar from "../assets/Tenz.png";
 export const channels = [
   {
     name: "心羽あん",
-    subs: 4385,
+    subs: 4390,
     bits: 50645,
     avatar: kokohaanAvatar,
     twitchUrl: "https://www.twitch.tv/kokohaan",
