1af561f68b18c0f38945bcb4a8e24c68c4d35236
Prem <prempawar241@gmail.com>
2026-07-23 11:36:49 +0530

    Updated subGfits from anchan 2 gift to special user

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -9,7 +9,7 @@ import TenZAvatar from "../assets/Tenz.png";
 export const channels = [
   {
     name: "心羽あん",
-    subs: 4421,
+    subs: 4423,
     bits: 50645,
     avatar: kokohaanAvatar,
     twitchUrl: "https://www.twitch.tv/kokohaan",
