980c4beb85cf708fdf43e494fe0fa9102b25bd86
Prem <prempawar241@gmail.com>
2026-07-28 17:33:33 +0530

    Updated subGfits from  mayasan

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -27,7 +27,7 @@ export const channels = [
   },
   {
     name: "我妻まや",
-    subs: 1452,
+    subs: 1487,
     bits: 1100,
     avatar: mayaAvatar,
     twitchUrl: "https://www.twitch.tv/agatsuma_maya",
