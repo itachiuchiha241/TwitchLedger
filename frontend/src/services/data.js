3056c2ebf81f0e9fa0c3fdeabb54b194b0da0fc6
Prem <prempawar241@gmail.com>
2026-08-21 12:05:35 +0530

    Updated subGfits from anchan

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -10,7 +10,7 @@ import harukochan from "../assets/harukochan.png";
 export const channels = [
   {
     name: "心羽あん",
-    subs: 4618,
+    subs: 4703,
     bits: 50645,
     avatar: kokohaanAvatar,
     twitchUrl: "https://www.twitch.tv/kokohaan",
