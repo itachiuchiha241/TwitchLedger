cb57d6a9ca7e27db9d4665cbb6254a2b052c906b
Prem <prempawar241@gmail.com>
2026-08-25 21:11:39 +0530

    Updated subGfits from  gabunomichan

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -19,7 +19,7 @@ export const channels = [
   },
   {
     name: "がぶ飲みちゃん",
-    subs: 4557,
+    subs: 4714,
     bits: 25000,
     avatar: gabuAvatar,
     twitchUrl: "https://www.twitch.tv/gabuchan_sy",
