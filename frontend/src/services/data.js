0f247e86e8930330416f95ca343beec3644ce96a
Prem <prempawar241@gmail.com>
2026-09-05 19:09:22 +0530

    Updated subGfits from anchan

--- frontend/src/services/data.js
+++ frontend/src/services/data.js
@@ -16,7 +16,7 @@ export const channels = [
   {
     name: "心羽あん",
     verified: true,
-    subs: 4849,
+    subs: 4869,
     bits: 50645,
     avatar: kokohaanAvatar,
     twitchUrl: "https://www.twitch.tv/kokohaan",
